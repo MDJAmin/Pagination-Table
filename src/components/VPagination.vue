de6e474fb27ef60cc7619ed061322816f82bb051
M_Αmin Jafarnejad <mohammadamindf@gmail.com>
2024-10-02 15:36:04 +0330

    Feat : pagination setup

--- src/components/VPagination.vue
+++ src/components/VPagination.vue
@@ -3,9 +3,21 @@
 </script>
 
 <template>
-
+  <div class="pagination">
+    <button> < </button>
+    <span></span>
+    <button> > </button>
+  </div>
 </template>
 
 <style scoped>
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+button {
+  margin: 0 5px;
+}
 </style>
