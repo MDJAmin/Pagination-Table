9aa9d6fe2dc97026751eb853d04c03e084fa61f4
M_Αmin Jafarnejad <mohammadamindf@gmail.com>
2024-10-02 15:38:20 +0330

    Refactor : Add functionality for buttons

--- src/components/VPagination.vue
+++ src/components/VPagination.vue
@@ -1,12 +1,39 @@
-<script setup>
-
+<script >
+export default {
+  props: {
+    currentPage: {
+      type: Number,
+      required: true
+    },
+    totalPages: {
+      type: Number,
+      required: true
+    },
+    itemsPerPage: {
+      type: Number,
+      default: 2
+    }
+  },
+  methods: {
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.$emit('update:currentPage', this.currentPage - 1);
+      }
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.$emit('update:currentPage', this.currentPage + 1);
+      }
+    }
+  }
+};
 </script>
 
 <template>
   <div class="pagination">
-    <button> < </button>
+    <button @click="prevPage" :disabled="currentPage === 1"> < </button>
     <span></span>
-    <button> > </button>
+    <button @click="nextPage" :disabled="currentPage === totalPages"> > </button>
   </div>
 </template>
 
